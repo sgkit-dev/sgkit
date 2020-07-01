b34b2708bca67b3f80222967827c1f9fce6753b0
Tom White <tom.e.white@gmail.com>
2020-07-01 10:16:32 +0100

    Move away from ALL_CAPS names. Pluralise dimension names.

--- sgkit/tests/test_api.py
+++ sgkit/tests/test_api.py
@@ -13,22 +13,24 @@ from sgkit import (
 def test_create_genotype_call_dataset():
     variant_contig_names = ["chr1"]
     variant_contig = np.array([0, 0], dtype="i1")
-    variant_pos = np.array([1000, 2000], dtype="i4")
+    variant_position = np.array([1000, 2000], dtype="i4")
     variant_alleles = np.array([["A", "C"], ["G", "A"]], dtype="S1")
     variant_id = np.array(["rs1", "rs2"], dtype=str)
     sample_id = np.array(["sample_1", "sample_2", "sample_3"], dtype=str)
-    call_gt = np.array(
+    call_genotype = np.array(
         [[[0, 0], [0, 1], [1, 0]], [[-1, 0], [0, -1], [-1, -1]]], dtype="i1"
     )
-    call_gt_phased = np.array([[True, True, False], [True, False, False]], dtype=bool)
+    call_genotype_phased = np.array(
+        [[True, True, False], [True, False, False]], dtype=bool
+    )
     ds = create_genotype_call_dataset(
         variant_contig_names,
         variant_contig,
-        variant_pos,
+        variant_position,
         variant_alleles,
         sample_id,
-        call_gt,
-        call_gt_phased=call_gt_phased,
+        call_genotype,
+        call_genotype_phased=call_genotype_phased,
         variant_id=variant_id,
     )
 
@@ -38,11 +40,11 @@ def test_create_genotype_call_dataset():
     assert DIM_ALLELE in ds.dims
 
     assert ds.attrs["contigs"] == variant_contig_names
-    assert_array_equal(ds["variant/CONTIG"], variant_contig)
-    assert_array_equal(ds["variant/POS"], variant_pos)
-    assert_array_equal(ds["variant/ALLELES"], variant_alleles)
-    assert_array_equal(ds["variant/ID"], variant_id)
-    assert_array_equal(ds["sample/ID"], sample_id)
-    assert_array_equal(ds["call/GT"], call_gt)
-    assert_array_equal(ds["call/GT_mask"], call_gt < 0)
-    assert_array_equal(ds["call/GT_phased"], call_gt_phased)
+    assert_array_equal(ds["variant/contig"], variant_contig)
+    assert_array_equal(ds["variant/position"], variant_position)
+    assert_array_equal(ds["variant/alleles"], variant_alleles)
+    assert_array_equal(ds["variant/id"], variant_id)
+    assert_array_equal(ds["sample/id"], sample_id)
+    assert_array_equal(ds["call/genotype"], call_genotype)
+    assert_array_equal(ds["call/genotype_mask"], call_genotype < 0)
+    assert_array_equal(ds["call/genotype_phased"], call_genotype_phased)
