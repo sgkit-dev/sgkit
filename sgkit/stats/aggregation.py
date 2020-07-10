656ecea34964e2c2ca1b458287bb66930311ba98
eric-czech <eric.allen.czech@gmail.com>
2020-07-10 05:58:24 -0400

    Making suggested changes

--- sgkit/stats/aggregation.py
+++ sgkit/stats/aggregation.py
@@ -11,13 +11,20 @@ def allele_count(ds: Dataset) -> DataArray:
         Genotype call dataset such as from
         `sgkit.create_genotype_call_dataset`
 
+    Returns
+    -------
+    DataArray
+        Allele counts with shape (variants, alleles) and values
+        corresponding to the number of non-missing occurrences
+        of each allele
+
     Examples
     --------
 
     >>> import sgkit as sg
     >>> from sgkit.testing import simulate_genotype_call_dataset
     >>> ds = simulate_genotype_call_dataset(n_variant=3, n_sample=2, seed=1)
-    # TODO: Should we expose visual convenience functions like this?
+    # TODO: Use solution for https://github.com/pystatgen/sgkit/issues/37 here instead
     >>> ds['call/genotype'].to_series().unstack().astype(str).apply('/'.join, axis=1).unstack()
     samples 0   1
     variants
@@ -33,22 +40,13 @@ def allele_count(ds: Dataset) -> DataArray:
         [2, 2],
         [4, 0]])
     Dimensions without coordinates: variants, alleles
-
-    Returns
-    -------
-    DataArray
-        Allele counts with shape (variants, alleles) and values
-        corresponding to the number of non-missing occurrences
-        of each allele
     """
-    # Collapse 3D calls into 2D array where calls are flattened into columns
-    gt = ds["call/genotype"].stack(calls=("samples", "ploidy"))
-    mask = ds["call/genotype_mask"].stack(calls=("samples", "ploidy"))
-
     # Count each allele index individually as a 1D vector and
     # restack into new alleles dimension with same order
+    gt, mask = ds["call/genotype"], ds["call/genotype_mask"]
     acs = [
-        xr.where(mask, 0, gt == i).sum(dim="calls") for i in range(ds.dims["alleles"])
+        xr.where(mask, 0, gt == i).sum(dim=("samples", "ploidy"))
+        for i in range(ds.dims["alleles"])
     ]
     ac = xr.concat(acs, dim="alleles")
     ac = ac.T.rename("variant/allele_count")
