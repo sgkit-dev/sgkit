fa1876c6cfa498b77fd11d016e990c96506128ec
Tom White <tom.e.white@gmail.com>
2020-07-20 17:45:48 +0100

    Move encode_array to utils module from sgkit-plink

--- sgkit/utils.py
+++ sgkit/utils.py
@@ -1,8 +1,8 @@
-from typing import Any, Set, Union
+from typing import Any, List, Set, Tuple, Union
 
 import numpy as np
 
-from .typing import DType
+from .typing import ArrayLike, DType
 
 
 def check_array_like(
@@ -31,3 +31,36 @@ def check_array_like(
                 raise ValueError
         elif ndim != a.ndim:
             raise ValueError
+
+
+def encode_array(x: ArrayLike) -> Tuple[ArrayLike, List[str]]:
+    """Encode array values as integers indexing unique values
+
+    The codes created for each unique element in the array correspond
+    to order of appearance, not the natural sort order for the array
+    dtype.
+
+    Examples
+    --------
+
+    >>> encode_array(['c', 'a', 'a', 'b'])
+    (array([0, 1, 1, 2]), array(['c', 'a', 'b'], dtype='<U1'))
+
+    Parameters
+    ----------
+    x : (M,) array-like
+        Array of elements to encode of any type
+
+    Returns
+    -------
+    indexes : (M,) ndarray
+        Encoded values as integer indices
+    values : ndarray
+        Unique values in original array in order of appearance
+    """
+    # argsort not implemented in dask: https://github.com/dask/dask/issues/4368
+    names, index, inverse = np.unique(x, return_index=True, return_inverse=True)
+    index = np.argsort(index)
+    rank = np.empty_like(index)
+    rank[index] = np.arange(len(index))
+    return rank[inverse], names[index]
