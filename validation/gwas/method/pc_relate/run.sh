560478794ec90fc05fac5826478ea662679caddf
Rafal Wojdyla <ravwojdyla@gmail.com>
2020-09-19 00:05:45 +0200

    Improve/fix the validation a bit
     * the order in the dockerfile matter a bit
     * fail run if any of the cmds fail along the way

--- validation/gwas/method/pc_relate/run.sh
+++ validation/gwas/method/pc_relate/run.sh
@@ -1,5 +1,8 @@
 #!/usr/bin/env bash
 
+set -o errexit
+set -o pipefail
+
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
 REPO_ROOT="$( cd "$DIR/../../../.." >/dev/null 2>&1 && pwd )"
 
