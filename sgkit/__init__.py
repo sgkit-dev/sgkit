920a759409c05712fa39aa2fdff06cef1f475e0b
Tom White <tom.e.white@gmail.com>
2020-09-22 16:38:34 +0100

    PBS

--- sgkit/__init__.py
+++ sgkit/__init__.py
@@ -18,7 +18,7 @@ from .stats.association import gwas_linear_regression
 from .stats.hwe import hardy_weinberg_test
 from .stats.pc_relate import pc_relate
 from .stats.pca import pca
-from .stats.popgen import Fst, Tajimas_D, divergence, diversity
+from .stats.popgen import Fst, Tajimas_D, divergence, diversity, pbs
 from .stats.preprocessing import filter_partial_calls
 from .stats.regenie import regenie
 from .testing import simulate_genotype_call_dataset
@@ -45,6 +45,7 @@ __all__ = [
     "divergence",
     "Fst",
     "Tajimas_D",
+    "pbs",
     "pc_relate",
     "simulate_genotype_call_dataset",
     "variables",
