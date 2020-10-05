fc455566edf8e5b7f21ecc047b7d4b6f578fcf1c
Tom White <tom.e.white@gmail.com>
2020-10-05 09:44:33 +0100

    Add error message for Windows

--- sgkit/io/vcf/__init__.py
+++ sgkit/io/vcf/__init__.py
@@ -1,3 +1,5 @@
+import platform
+
 try:
     from .vcf_partition import partition_into_regions  # noqa: F401
     from .vcf_reader import vcf_to_zarr, vcf_to_zarrs, zarrs_to_dataset  # noqa: F401
@@ -9,9 +11,15 @@ try:
         "zarrs_to_dataset",
     ]
 except ImportError as e:
-    msg = (
-        "sgkit-vcf requirements are not installed.\n\n"
-        "Please install them via pip :\n\n"
-        "  pip install 'git+https://github.com/pystatgen/sgkit#egg=sgkit[vcf]'"
-    )
+    if platform.system() == "Windows":
+        msg = (
+            "sgkit-vcf is not supported on Windows.\n"
+            "Please see the sgkit documentation for details and workarounds."
+        )
+    else:
+        msg = (
+            "sgkit-vcf requirements are not installed.\n\n"
+            "Please install them via pip :\n\n"
+            "  pip install 'git+https://github.com/pystatgen/sgkit#egg=sgkit[vcf]'"
+        )
     raise ImportError(str(e) + "\n\n" + msg) from e
