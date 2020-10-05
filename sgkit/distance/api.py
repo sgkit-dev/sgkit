da0acc9956723f1e85c969662ebe501100941db7
Amit Kumar <dtu.amit@gmail.com>
2020-10-05 20:28:48 +0200

    Implement pairwise distance calculation

---
+++ sgkit/distance/api.py
@@ -0,0 +1,74 @@
+import typing
+
+import numpy as np
+
+from sgkit.distance import metrics
+from sgkit.distance.core import pairwise
+from sgkit.typing import ArrayLike
+
+
+def pdist(
+    x: ArrayLike,
+    metric: str = "euclidean",
+    chunks: typing.Optional[typing.Tuple[int, int]] = None,
+) -> np.ndarray:
+    """Calculates the pairwise distance between all pairs of vectors in the
+    given two dimensional array x. The API is similar to:
+    ``scipy.spatial.distance.pdist``.
+
+
+    Parameters
+    ----------
+    x
+        [array-like, shape: (M, N)]
+        An array like two dimensional matrix
+    metric
+        The distance metric to use. The distance function can be
+        'euclidean' or 'correlation'
+    chunks
+        The chunksize for the given array, if x is of type ndarray
+
+    Returns
+    -------
+
+    [array-like, shape: (M, N)]
+    A two dimensional distance matrix, which will be symmetric. The dimension
+    will be (M, N). The (i, j) position in the resulting array
+    (matrix) denotes the distance between ith and jth vectors.
+
+    Examples
+    --------
+
+    >>> from sgkit.distance.api import pdist
+    >>> import dask.array as da
+    >>> x = da.array([[6, 4, 1,], [4, 5, 2], [9, 7, 3]]).rechunk(2, 2)
+    >>> pdist(x, metric='euclidean')
+    array([[0.        , 2.44948974, 4.69041576],
+           [2.44948974, 0.        , 5.47722558],
+           [4.69041576, 5.47722558, 0.        ]])
+
+    >>> import numpy as np
+    >>> x = np.array([[6, 4, 1,], [4, 5, 2], [9, 7, 3]])
+    >>> pdist(x, metric='euclidean', chunks=(2, 2))
+    array([[0.        , 2.44948974, 4.69041576],
+           [2.44948974, 0.        , 5.47722558],
+           [4.69041576, 5.47722558, 0.        ]])
+    """
+
+    try:
+        distance_map_ufunc = getattr(metrics, f"{metric}_map")
+        distance_reduce_ufunc = getattr(metrics, f"{metric}_reduce")
+    except AttributeError:
+        raise NotImplementedError(
+            f"Given metric: {metric} is not implemented. "
+            f"Available metrics are: {metrics.N_MAP_PARAM.keys()}"
+        )
+
+    x_corr = pairwise(
+        x,
+        distance_map_ufunc,
+        distance_reduce_ufunc,
+        metrics.N_MAP_PARAM[metric],
+        chunks,
+    )
+    return x_corr.compute()
---
+++ sgkit/tests/test_distance.py
@@ -0,0 +1,45 @@
+import dask.array as da
+import numpy as np
+import pytest
+from scipy.spatial.distance import pdist as scipy_pdist  # type: ignore
+from scipy.spatial.distance import squareform
+
+from sgkit.distance.api import pdist
+from sgkit.typing import ArrayLike
+
+
+def get_vectors(array_type: str = "da") -> ArrayLike:
+    if array_type == "da":
+        rs = da.random.RandomState(0)
+        x = rs.randint(0, 3, size=(18, 40), dtype="i1").rechunk((6, 10))
+    else:
+        x = np.random.rand(18, 40)
+    return x
+
+
+def test_distance_correlation() -> None:
+    x = get_vectors()
+    distance_matrix = pdist(x, metric="correlation")
+    np.testing.assert_almost_equal(distance_matrix, np.corrcoef(x).compute())
+
+
+def test_distance_euclidean() -> None:
+    x = get_vectors()
+    distance_matrix = pdist(x, metric="euclidean")
+    distance_array = scipy_pdist(x)
+    expected_matrix = squareform(distance_array)
+    np.testing.assert_almost_equal(distance_matrix, expected_matrix)
+
+
+def test_distance_ndarray() -> None:
+    x = get_vectors(array_type="np")
+    distance_matrix = pdist(x, metric="euclidean", chunks=(6, 10))
+    distance_array = scipy_pdist(x)
+    expected_matrix = squareform(distance_array)
+    np.testing.assert_almost_equal(distance_matrix, expected_matrix)
+
+
+def test_undefined_metric() -> None:
+    x = get_vectors(array_type="np")
+    with pytest.raises(NotImplementedError):
+        pdist(x, metric="not-implemented-metric", chunks=(6, 10))
