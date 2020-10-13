c2e3cb3314d1097d2075a172bb1dbb38a7f19c31
Tom White <tom.e.white@gmail.com>
2020-10-13 11:00:47 +0100

    Add vcfzarr_to_zarr

--- sgkit/io/vcfzarr_reader.py
+++ sgkit/io/vcfzarr_reader.py
@@ -1,10 +1,16 @@
+import tempfile
+from pathlib import Path
+from typing import List, Optional
+
 import dask.array as da
 import xarray as xr
 import zarr
 
+from sgkit.io.utils import concatenate_and_rechunk
+
 from ..model import DIM_VARIANT, create_genotype_call_dataset
 from ..typing import ArrayLike, PathType
-from ..utils import encode_array
+from ..utils import encode_array, max_str_len
 
 
 def _ensure_2d(arr: ArrayLike) -> ArrayLike:
@@ -42,11 +48,142 @@ def read_vcfzarr(path: PathType) -> xr.Dataset:
 
     vcfzarr = zarr.open_group(str(path), mode="r")
 
-    # Index the contig names
-    variants_chrom = da.from_zarr(vcfzarr["variants/CHROM"]).astype(str)
-    variant_contig, variant_contig_names = encode_array(variants_chrom.compute())
-    variant_contig = variant_contig.astype("int16")
-    variant_contig_names = list(variant_contig_names)
+    # don't fix strings since it requires a pass over the whole dataset
+    return _vcfzarr_to_dataset(vcfzarr, fix_strings=False)
+
+
+def vcfzarr_to_zarr(
+    input: PathType,
+    output: PathType,
+    *,
+    contigs: Optional[List[str]] = None,
+    grouped_by_contig: bool = False,
+    consolidated: bool = False,
+    tempdir: Optional[PathType] = None,
+) -> None:
+    """Convert VCF Zarr files created using scikit-allel to a single Zarr on-disk store in sgkit Xarray format.
+
+    Parameters
+    ----------
+    input
+        Path to the input Zarr file.
+    output
+        Path to the ouput Zarr file.
+    contigs
+        The contigs to convert. By default all contigs are converted.
+    grouped_by_contig
+        Whether there is one group for each contig in the Zarr file, by default False.
+    consolidated
+        Whether the Zarr file has consolidated metadata, by default False.
+    tempdir
+        Temporary directory where intermediate files are stored. The default None means
+        use the system default temporary directory.
+    """
+
+    if consolidated:
+        vcfzarr = zarr.open_consolidated(str(input), mode="r")
+    else:
+        vcfzarr = zarr.open_group(str(input), mode="r")
+
+    if not grouped_by_contig:
+        ds = _vcfzarr_to_dataset(vcfzarr)
+        ds.to_zarr(str(output))
+
+    else:
+        # read each contig separately, concatenate, rechunk, then save to zarr
+
+        contigs = contigs or list(vcfzarr.group_keys())
+
+        # Index the contig names
+        _, variant_contig_names = encode_array(contigs)
+        variant_contig_names = list(variant_contig_names)
+
+        vars_to_rechunk = []
+        vars_to_copy = []
+
+        with tempfile.TemporaryDirectory(
+            prefix="vcfzarr_to_zarr_", suffix=".zarr", dir=tempdir
+        ) as tmpdir:
+            zarr_files = []
+            for i, contig in enumerate(contigs):
+                # convert contig group to zarr and save in tmpdir
+                ds = _vcfzarr_to_dataset(vcfzarr[contig], contig, variant_contig_names)
+                if i == 0:
+                    for (var, arr) in ds.data_vars.items():
+                        if arr.dims[0] == "variants":
+                            vars_to_rechunk.append(var)
+                        else:
+                            vars_to_copy.append(var)
+
+                contig_zarr_file = Path(tmpdir) / contig
+                ds.to_zarr(contig_zarr_file)
+
+                zarr_files.append(contig_zarr_file)
+
+            zarr_groups = [zarr.open_group(str(f)) for f in zarr_files]
+
+            first_zarr_group = zarr_groups[0]
+
+            with zarr.open_group(str(output)) as output_zarr:
+
+                var_to_attrs = {}  # attributes to copy
+                delayed = []  # do all the rechunking operations in one computation
+                for var in vars_to_rechunk:
+                    var_to_attrs[var] = first_zarr_group[var].attrs.asdict()
+                    dtype = None
+                    if var == "variant_id":
+                        kind = first_zarr_group[var].dtype.kind
+                        max_len = _get_max_len(zarr_groups, "max_variant_id_length")
+                        dtype = f"{kind}{max_len}"
+                    elif var == "variant_allele":
+                        kind = first_zarr_group[var].dtype.kind
+                        max_len = _get_max_len(zarr_groups, "max_variant_allele_length")
+                        dtype = f"{kind}{max_len}"
+
+                    arr = concatenate_and_rechunk(
+                        [group[var] for group in zarr_groups], dtype=dtype
+                    )
+                    d = arr.to_zarr(
+                        str(output),
+                        component=var,
+                        overwrite=True,
+                        compute=False,
+                        fill_value=None,
+                    )
+                    delayed.append(d)
+                da.compute(*delayed)
+
+                # copy variables that are not rechunked (e.g. sample_id)
+                for var in vars_to_copy:
+                    output_zarr[var] = first_zarr_group[var]
+                    output_zarr[var].attrs.update(first_zarr_group[var].attrs)
+
+                # copy attributes
+                output_zarr.attrs.update(first_zarr_group.attrs)
+                for (var, attrs) in var_to_attrs.items():
+                    output_zarr[var].attrs.update(attrs)
+
+
+def _vcfzarr_to_dataset(
+    vcfzarr: zarr.Array,
+    contig: Optional[str] = None,
+    variant_contig_names: Optional[List[str]] = None,
+    fix_strings: bool = True,
+) -> xr.Dataset:
+
+    variant_position = da.from_zarr(vcfzarr["variants/POS"])
+
+    if contig is None:
+        # Get the contigs from variants/CHROM
+        variants_chrom = da.from_zarr(vcfzarr["variants/CHROM"]).astype(str)
+        variant_contig, variant_contig_names = encode_array(variants_chrom.compute())
+        variant_contig = variant_contig.astype("int16")
+        variant_contig_names = list(variant_contig_names)
+    else:
+        # Single contig: contig names were passed in
+        assert variant_contig_names is not None
+        contig_index = variant_contig_names.index(contig)
+        variant_contig = da.full_like(variant_position, contig_index)
 
     # For variant alleles, combine REF and ALT into a single array
     variants_ref = da.from_zarr(vcfzarr["variants/REF"])
@@ -54,23 +191,51 @@ def read_vcfzarr(path: PathType) -> xr.Dataset:
     variant_allele = da.concatenate(
         [_ensure_2d(variants_ref), _ensure_2d(variants_alt)], axis=1
     )
+    # rechunk so there's a single chunk in alleles axis
+    variant_alleles = variant_alleles.rechunk((None, variant_alleles.shape[1]))
 
-    variants_id = da.from_zarr(vcfzarr["variants/ID"]).astype(str)
+    if "variants/ID" in vcfzarr:
+        variants_id = da.from_zarr(vcfzarr["variants/ID"]).astype(str)
+    else:
+        variants_id = None
 
     ds = create_genotype_call_dataset(
         variant_contig_names=variant_contig_names,
         variant_contig=variant_contig,
-        variant_position=da.from_zarr(vcfzarr["variants/POS"]),
-        variant_allele=variant_allele,
+        variant_position=variant_position,
+        variant_alleles=variant_alleles,
         sample_id=da.from_zarr(vcfzarr["samples"]).astype(str),
         call_genotype=da.from_zarr(vcfzarr["calldata/GT"]),
         variant_id=variants_id,
     )
 
     # Add a mask for variant ID
-    ds["variant_id_mask"] = (
-        [DIM_VARIANT],
-        variants_id == ".",
-    )
+    if variants_id is not None:
+        ds["variant_id_mask"] = (
+            [DIM_VARIANT],
+            variants_id == ".",
+        )
+
+    # Fix string types to include length
+    if fix_strings:
+        for (var, arr) in ds.data_vars.items():
+            kind = arr.dtype.kind
+            if kind in ["O", "U", "S"]:
+                # Compute fixed-length string dtype for array
+                if kind == "O":
+                    kind = "S"
+                max_len = max_str_len(arr).values
+                dt = f"{kind}{max_len}"
+                ds[var] = arr.astype(dt)  # type: ignore[no-untyped-call]
+
+                if var == "variant_id":
+                    ds.attrs["max_variant_id_length"] = max_len
+                if var == "variant_allele":
+                    ds.attrs["max_variant_allele_length"] = max_len
 
     return ds
+
+
+def _get_max_len(zarr_groups: List[zarr.Group], attr_name: str) -> int:
+    max_len: int = max([group.attrs[attr_name] for group in zarr_groups])
+    return max_len
--- sgkit/tests/test_vcfzarr_reader.py
+++ sgkit/tests/test_vcfzarr_reader.py
@@ -1,8 +1,15 @@
 import numpy as np
+import pytest
+import xarray as xr
 from numpy.testing import assert_array_equal
 
 from sgkit import read_vcfzarr
-from sgkit.io.vcfzarr_reader import _ensure_2d
+from sgkit.io.vcfzarr_reader import _ensure_2d, vcfzarr_to_zarr
+
+
+def test_ensure_2d():
+    assert_array_equal(_ensure_2d(np.array([0, 2, 1])), np.array([[0], [2], [1]]))
+    assert_array_equal(_ensure_2d(np.array([[0], [2], [1]])), np.array([[0], [2], [1]]))
 
 
 def test_read_vcfzarr(shared_datadir):
@@ -65,6 +72,85 @@ def test_read_vcfzarr(shared_datadir):
     assert "call_genotype_phased" not in ds
 
 
-def test_ensure_2d():
-    assert_array_equal(_ensure_2d(np.array([0, 2, 1])), np.array([[0], [2], [1]]))
-    assert_array_equal(_ensure_2d(np.array([[0], [2], [1]])), np.array([[0], [2], [1]]))
+@pytest.mark.parametrize(
+    "vcfzarr_filename, grouped_by_contig",
+    [("sample.vcf.zarr.zip", False), ("sample-grouped.vcf.zarr.zip", True)],
+)
+def test_vcfzarr_to_zarr(shared_datadir, tmp_path, vcfzarr_filename, grouped_by_contig):
+    # The file sample-grouped.vcf.zarr.zip was created by running the following
+    # in a python session with the scikit-allel package installed.
+    #
+    # import allel
+    # for contig in ["19", "20", "X"]:
+    #   allel.vcf_to_zarr("sample.vcf", "sample-grouped.vcf.zarr", group=contig, region=contig)
+    #
+    # Then (in a shell):
+    # (cd sample-grouped.vcf.zarr; zip -r ../sample-grouped.vcf.zarr.zip .)
+
+    path = shared_datadir / vcfzarr_filename
+    output = tmp_path.joinpath("vcf.zarr").as_posix()
+    vcfzarr_to_zarr(path, output, grouped_by_contig=grouped_by_contig)
+
+    ds = xr.open_zarr(output)  # type: ignore[no-untyped-call]
+
+    # Note that variant_allele values are byte strings, not unicode strings (unlike for read_vcfzarr)
+    # We should make the two consistent.
+
+    assert ds.attrs["contigs"] == ["19", "20", "X"]
+    assert_array_equal(ds["variant_contig"], [0, 0, 1, 1, 1, 1, 1, 1, 2])
+    assert_array_equal(
+        ds["variant_position"],
+        [111, 112, 14370, 17330, 1110696, 1230237, 1234567, 1235237, 10],
+    )
+    assert_array_equal(
+        ds["variant_allele"],
+        [
+            [b"A", b"C", b"", b""],
+            [b"A", b"G", b"", b""],
+            [b"G", b"A", b"", b""],
+            [b"T", b"A", b"", b""],
+            [b"A", b"G", b"T", b""],
+            [b"T", b"", b"", b""],
+            [b"G", b"GA", b"GAC", b""],
+            [b"T", b"", b"", b""],
+            [b"AC", b"A", b"ATG", b"C"],
+        ],
+    )
+    assert_array_equal(
+        ds["variant_id"],
+        [
+            ".",
+            ".",
+            "rs6054257",
+            ".",
+            "rs6040355",
+            ".",
+            "microsat1",
+            ".",
+            "rsTest",
+        ],
+    )
+    assert_array_equal(
+        ds["variant_id_mask"],
+        [True, True, False, True, False, True, False, True, False],
+    )
+
+    assert_array_equal(ds["sample_id"], ["NA00001", "NA00002", "NA00003"])
+
+    call_genotype = np.array(
+        [
+            [[0, 0], [0, 0], [0, 1]],
+            [[0, 0], [0, 0], [0, 1]],
+            [[0, 0], [1, 0], [1, 1]],
+            [[0, 0], [0, 1], [0, 0]],
+            [[1, 2], [2, 1], [2, 2]],
+            [[0, 0], [0, 0], [0, 0]],
+            [[0, 1], [0, 2], [-1, -1]],
+            [[0, 0], [0, 0], [-1, -1]],
+            [[0, -1], [0, 1], [0, 2]],
+        ],
+        dtype="i1",
+    )
+    assert_array_equal(ds["call_genotype"], call_genotype)
+    assert_array_equal(ds["call_genotype_mask"], call_genotype < 0)
+    assert "call_genotype_phased" not in ds
