eea1ac17d6a654ccaf6ed5ea3c9465d83e6097de
Amit Kumar <dtu.amit@gmail.com>
2020-10-15 21:58:22 +0200

    Add tests for missing values

--- sgkit/tests/test_distance.py
+++ sgkit/tests/test_distance.py
@@ -1,3 +1,6 @@
+import typing
+from random import randint
+
 import dask.array as da
 import numpy as np
 import pytest
@@ -11,9 +14,17 @@ from sgkit.typing import ArrayLike
 def get_vectors(array_type: str = "da") -> ArrayLike:
     if array_type == "da":
         rs = da.random.RandomState(0)
-        x = rs.randint(0, 3, size=(18, 40), dtype="i1").rechunk((6, 10))
+        x = rs.randint(0, 3, size=(5, 5), dtype="i1").rechunk((2, 2))
     else:
-        x = np.random.rand(18, 40)
+        x = np.random.rand(5, 5)
+    return x
+
+
+def fill_invalid_indices_with_invalid_value(
+    x: ArrayLike, invalid_indices: typing.Set[int], invalid_value: float = np.nan
+) -> ArrayLike:
+    for i in invalid_indices:
+        x[i] = invalid_value
     return x
 
 
@@ -33,11 +44,17 @@ def test_distance_euclidean() -> None:
 
 def test_distance_missing_values() -> None:
     x = get_vectors(array_type="np")
-    x[5][6] = np.nan
-    x[10][1] = np.nan
+    nan_indices = set([randint(0, x.shape[1] - 1) for _ in range(5)])
+
+    for k in nan_indices:
+        x[k][randint(0, x.shape[1] - 1)] = np.nan
+
     distance_matrix = pdist(x)
     distance_array = scipy_pdist(x)
     expected_matrix = squareform(distance_array)
+    expected_matrix = fill_invalid_indices_with_invalid_value(
+        expected_matrix, nan_indices, np.nan
+    )
     np.testing.assert_almost_equal(distance_matrix, expected_matrix)
 
 
