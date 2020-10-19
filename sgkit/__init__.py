0b9505620ceaff52e716c9eca8b5a65de91db71e
tim millar <timmymillar@gmail.com>
2020-10-20 08:52:00 +1300

    Add filter_partial_calls function #223

--- sgkit/__init__.py
+++ sgkit/__init__.py
@@ -14,6 +14,7 @@ from .stats.hwe import hardy_weinberg_test
 from .stats.pc_relate import pc_relate
 from .stats.pca import pca
 from .stats.popgen import Fst, Tajimas_D, divergence, diversity
+from .stats.preprocessing import filter_partial_calls
 from .stats.regenie import regenie
 from .testing import simulate_genotype_call_dataset
 
@@ -27,6 +28,7 @@ __all__ = [
     "count_call_alleles",
     "create_genotype_dosage_dataset",
     "display_genotypes",
+    "filter_partial_calls",
     "gwas_linear_regression",
     "read_vcfzarr",
     "regenie",
