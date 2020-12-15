f8cc71f56e4fef6057053fab61bfe9acb724c170
Tom White <tom.e.white@gmail.com>
2020-12-15 12:14:00 +0000

    Use fsspec in load_dataset and save_dataset #395

--- sgkit/tests/io/test_dataset.py
+++ sgkit/tests/io/test_dataset.py
@@ -1,3 +1,4 @@
+import pytest
 import xarray as xr
 from xarray import Dataset
 
@@ -11,14 +12,22 @@ def assert_identical(ds1: Dataset, ds2: Dataset) -> None:
     assert all([ds1[v].dtype == ds2[v].dtype for v in ds1.data_vars])
 
 
-def test_save_and_load_dataset(tmp_path):
-    path = str(tmp_path / "ds.zarr")
+@pytest.mark.parametrize(
+    "is_path",
+    [True, False],
+)
+def test_save_and_load_dataset(tmp_path, is_path):
+    path = tmp_path / "ds.zarr"
+    if not is_path:
+        path = str(path)
     ds = simulate_genotype_call_dataset(n_variant=10, n_sample=10)
     save_dataset(ds, path)
     ds2 = load_dataset(path)
     assert_identical(ds, ds2)
 
     # save and load again to test https://github.com/pydata/xarray/issues/4386
-    path2 = str(tmp_path / "ds2.zarr")
+    path2 = tmp_path / "ds2.zarr"
+    if not is_path:
+        path2 = str(path2)
     save_dataset(ds2, path2)
     assert_identical(ds, load_dataset(path2))
