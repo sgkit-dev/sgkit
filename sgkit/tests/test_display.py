0e13cb8f35fcb81e4f8ec2798d5dbdb0fd183cd0
Tom White <tom.e.white@gmail.com>
2021-02-16 10:14:38 +0000

    Use dedent to improve formatting of expected display text in tests

--- sgkit/tests/test_display.py
+++ sgkit/tests/test_display.py
@@ -1,3 +1,5 @@
+from textwrap import dedent
+
 import numpy as np
 import pytest
 
@@ -9,16 +11,13 @@ from sgkit.testing import simulate_genotype_call_dataset
 def test_display_genotypes():
     ds = simulate_genotype_call_dataset(n_variant=3, n_sample=3, seed=0)
     disp = display_genotypes(ds)
-    assert (
-        str(disp)
-        == """
-samples    S0   S1   S2
-variants               
-0         0/0  1/0  1/0
-1         0/1  1/0  0/1
-2         0/0  1/0  1/1
-""".strip()  # noqa: W291
-    )
+    expected = """\
+        samples    S0   S1   S2
+        variants               
+        0         0/0  1/0  1/0
+        1         0/1  1/0  0/1
+        2         0/0  1/0  1/1"""  # noqa: W291
+    assert str(disp) == dedent(expected)
 
     expected_html = """<table border="1" class="dataframe">
   <thead>
@@ -65,16 +64,13 @@ def test_display_genotypes__variant_ids():
     ds["variant_id"] = (["variants"], np.array(["V0", "V1", "V2"]))
     ds["variant_id_mask"] = (["variants"], np.array([False, False, False]))
     disp = display_genotypes(ds)
-    assert (
-        str(disp)
-        == """
-samples    S0   S1   S2
-variants               
-V0        0/0  1/0  1/0
-V1        0/1  1/0  0/1
-V2        0/0  1/0  1/1
-""".strip()  # noqa: W291
-    )
+    expected = """\
+        samples    S0   S1   S2
+        variants               
+        V0        0/0  1/0  1/0
+        V1        0/1  1/0  0/1
+        V2        0/0  1/0  1/1"""  # noqa: W291
+    assert str(disp) == dedent(expected)
 
 
 def test_display_genotypes__missing_variant_ids():
@@ -83,16 +79,13 @@ def test_display_genotypes__missing_variant_ids():
     ds["variant_id"] = (["variants"], np.array(["."] * 3))
     ds["variant_id_mask"] = (["variants"], np.array([False] * 3))
     disp = display_genotypes(ds)
-    assert (
-        str(disp)
-        == """
-samples    S0   S1   S2
-variants               
-0         0/0  1/0  1/0
-1         0/1  1/0  0/1
-2         0/0  1/0  1/1
-""".strip()  # noqa: W291
-    )
+    expected = """\
+        samples    S0   S1   S2
+        variants               
+        0         0/0  1/0  1/0
+        1         0/1  1/0  0/1
+        2         0/0  1/0  1/1"""  # noqa: W291
+    assert str(disp) == dedent(expected)
 
 
 def test_display_genotypes__duplicate_variant_ids():
@@ -101,78 +94,66 @@ def test_display_genotypes__duplicate_variant_ids():
     ds["variant_id"] = (["variants"], np.array(["V0", "V1", "V1"]))
     ds["variant_id_mask"] = (["variants"], np.array([False, False, False]))
     disp = display_genotypes(ds)
-    assert (
-        str(disp)
-        == """
-samples    S0   S1   S2
-variants               
-0         0/0  1/0  1/0
-1         0/1  1/0  0/1
-2         0/0  1/0  1/1
-""".strip()  # noqa: W291
-    )
+    expected = """\
+        samples    S0   S1   S2
+        variants               
+        0         0/0  1/0  1/0
+        1         0/1  1/0  0/1
+        2         0/0  1/0  1/1"""  # noqa: W291
+    assert str(disp) == dedent(expected)
 
 
 def test_display_genotypes__truncated_rows():
     ds = simulate_genotype_call_dataset(n_variant=10, n_sample=10, seed=0)
     disp = display_genotypes(ds, max_variants=4, max_samples=10)
-    assert (
-        str(disp)
-        == """
-samples    S0   S1   S2   S3   S4   S5   S6   S7   S8   S9
-variants                                                  
-0         0/0  1/0  1/0  0/1  1/0  0/1  0/0  1/0  1/1  0/0
-1         1/0  0/1  1/0  1/1  1/1  1/0  1/0  0/0  1/0  1/1
-...       ...  ...  ...  ...  ...  ...  ...  ...  ...  ...
-8         0/1  0/0  1/0  0/1  0/1  1/0  1/0  0/1  1/0  1/0
-9         1/1  0/1  1/0  0/1  1/0  1/1  0/1  1/0  1/1  1/0
+    expected = """\
+        samples    S0   S1   S2   S3   S4   S5   S6   S7   S8   S9
+        variants                                                  
+        0         0/0  1/0  1/0  0/1  1/0  0/1  0/0  1/0  1/1  0/0
+        1         1/0  0/1  1/0  1/1  1/1  1/0  1/0  0/0  1/0  1/1
+        ...       ...  ...  ...  ...  ...  ...  ...  ...  ...  ...
+        8         0/1  0/0  1/0  0/1  0/1  1/0  1/0  0/1  1/0  1/0
+        9         1/1  0/1  1/0  0/1  1/0  1/1  0/1  1/0  1/1  1/0
 
-[10 rows x 10 columns]
-""".strip()  # noqa: W291
-    )
+        [10 rows x 10 columns]"""  # noqa: W291
+    assert str(disp) == dedent(expected)
 
 
 def test_display_genotypes__truncated_columns():
     ds = simulate_genotype_call_dataset(n_variant=10, n_sample=10, seed=0)
     disp = display_genotypes(ds, max_variants=10, max_samples=4)
-    assert (
-        str(disp)
-        == """
-samples    S0   S1  ...   S8   S9
-variants            ...          
-0         0/0  1/0  ...  1/1  0/0
-1         1/0  0/1  ...  1/0  1/1
-2         1/1  1/1  ...  0/0  1/0
-3         0/1  0/0  ...  1/0  0/0
-4         0/1  0/0  ...  0/0  1/1
-5         1/1  1/0  ...  0/0  1/0
-6         1/1  0/0  ...  1/0  0/1
-7         1/0  0/1  ...  0/1  0/0
-8         0/1  0/0  ...  1/0  1/0
-9         1/1  0/1  ...  1/1  1/0
+    expected = """\
+        samples    S0   S1  ...   S8   S9
+        variants            ...          
+        0         0/0  1/0  ...  1/1  0/0
+        1         1/0  0/1  ...  1/0  1/1
+        2         1/1  1/1  ...  0/0  1/0
+        3         0/1  0/0  ...  1/0  0/0
+        4         0/1  0/0  ...  0/0  1/1
+        5         1/1  1/0  ...  0/0  1/0
+        6         1/1  0/0  ...  1/0  0/1
+        7         1/0  0/1  ...  0/1  0/0
+        8         0/1  0/0  ...  1/0  1/0
+        9         1/1  0/1  ...  1/1  1/0
 
-[10 rows x 10 columns]
-""".strip()  # noqa: W291
-    )
+        [10 rows x 10 columns]"""  # noqa: W291
+    assert str(disp) == dedent(expected)
 
 
 def test_display_genotypes__truncated_rows_and_columns():
     ds = simulate_genotype_call_dataset(n_variant=10, n_sample=10, seed=0)
     disp = display_genotypes(ds, max_variants=4, max_samples=4)
-    assert (
-        str(disp)
-        == """
-samples    S0   S1  ...   S8   S9
-variants            ...          
-0         0/0  1/0  ...  1/1  0/0
-1         1/0  0/1  ...  1/0  1/1
-...       ...  ...  ...  ...  ...
-8         0/1  0/0  ...  1/0  1/0
-9         1/1  0/1  ...  1/1  1/0
+    expected = """\
+        samples    S0   S1  ...   S8   S9
+        variants            ...          
+        0         0/0  1/0  ...  1/1  0/0
+        1         1/0  0/1  ...  1/0  1/1
+        ...       ...  ...  ...  ...  ...
+        8         0/1  0/0  ...  1/0  1/0
+        9         1/1  0/1  ...  1/1  1/0
 
-[10 rows x 10 columns]
-""".strip()  # noqa: W291
-    )
+        [10 rows x 10 columns]"""  # noqa: W291
+    assert str(disp) == dedent(expected)
 
     expected_html = """<table border="1" class="dataframe">
   <thead>
@@ -243,20 +224,17 @@ variants            ...
 def test_display_genotypes__large():
     ds = simulate_genotype_call_dataset(n_variant=100_000, n_sample=1000, seed=0)
     disp = display_genotypes(ds, max_variants=4, max_samples=4)
-    assert (
-        str(disp)
-        == """
-samples    S0   S1  ... S998 S999
-variants            ...          
-0         0/0  1/0  ...  0/1  1/1
-1         1/1  1/1  ...  0/1  1/1
-...       ...  ...  ...  ...  ...
-99998     0/1  1/1  ...  1/0  0/1
-99999     1/0  1/0  ...  1/0  1/0
+    expected = """\
+        samples    S0   S1  ... S998 S999
+        variants            ...          
+        0         0/0  1/0  ...  0/1  1/1
+        1         1/1  1/1  ...  0/1  1/1
+        ...       ...  ...  ...  ...  ...
+        99998     0/1  1/1  ...  1/0  0/1
+        99999     1/0  1/0  ...  1/0  1/0
 
-[100000 rows x 1000 columns]
-""".strip()  # noqa: W291
-    )
+        [100000 rows x 1000 columns]"""  # noqa: W291
+    assert str(disp) == dedent(expected)
 
 
 def test_truncate_fails_with_only_one_dimension():
