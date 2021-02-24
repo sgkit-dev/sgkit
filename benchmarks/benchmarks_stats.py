a3b9202851b86a6bb8b7c0e32cefe24de01f6c3b
Amit Kumar <dtu.amit@gmail.com>
2021-02-24 16:36:18 +0000

    Increase dataset size

--- benchmarks/benchmarks_stats.py
+++ benchmarks/benchmarks_stats.py
@@ -3,32 +3,30 @@
 import numpy as np
 import xarray as xr
 
-from sgkit import count_call_alleles, count_cohort_alleles
-from sgkit.tests.test_aggregation import get_dataset
+from sgkit import (
+    count_call_alleles,
+    count_cohort_alleles,
+    simulate_genotype_call_dataset,
+)
 
 
-def time_count_call_alleles() -> None:
-    count_call_alleles(
-        get_dataset(
-            [
-                [[0, 0], [0, 0], [0, 0]],
-                [[0, 0], [0, 0], [0, 1]],
-                [[1, 1], [0, 1], [1, 0]],
-                [[1, 1], [1, 1], [1, 1]],
-            ]
+class TimeSuite:
+    def setup(self) -> None:
+        self.count_call_alleles_ds = simulate_genotype_call_dataset(
+            n_variant=100_000, n_sample=1000
         )
-    )
+        self.count_cohort_alleles_ds = simulate_genotype_call_dataset(
+            n_variant=100_000, n_sample=1000
+        )
+        sample_cohort = np.repeat(
+            [0, 1], self.count_cohort_alleles_ds.dims["samples"] // 2
+        )
+        self.count_cohort_alleles_ds["sample_cohort"] = xr.DataArray(
+            sample_cohort, dims="samples"
+        )
 
+    def time_count_call_alleles(self) -> None:
+        count_call_alleles(self.count_call_alleles_ds)
 
-def time_count_cohort_alleles() -> None:
-    ds = get_dataset(
-        [
-            [[0, 0], [0, 0], [0, 0], [0, 0]],
-            [[0, 0], [0, 0], [0, 1], [0, 1]],
-            [[1, 1], [0, 1], [1, 0], [1, 0]],
-            [[1, 1], [1, 1], [1, 1], [1, 1]],
-        ]
-    )
-
-    ds["sample_cohort"] = xr.DataArray(np.array([0, 1, 1, -1]), dims="samples")
-    count_cohort_alleles(ds)
+    def time_count_cohort_alleles(self) -> None:
+        count_cohort_alleles(self.count_cohort_alleles_ds)
