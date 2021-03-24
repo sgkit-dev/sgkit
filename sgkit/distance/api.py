321a1b22e46f4d108d5a9b44d7794510afdaa858
Amit Kumar <dtu.amit@gmail.com>
2021-03-24 16:48:07 +0000

    remove cuda sync and cleanups

--- sgkit/distance/api.py
+++ sgkit/distance/api.py
@@ -3,8 +3,6 @@ import typing
 import dask.array as da
 import numpy as np
 
-from numba import cuda
-from numba.cuda import CudaSupportError
 from typing_extensions import Literal
 
 from sgkit.distance import metrics
