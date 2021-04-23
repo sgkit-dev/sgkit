Fix issues in Tajima's D calculation

sgkit/stats/popgen.py
@@ -554,8 +554,10 @@ def Tajimas_D(
 
     ac = ds[variant_allele_count]
 
-    # count segregating
-    S = ((ac > 0).sum(axis=1) > 1).sum()
+    # count segregating. Note that this uses the definition in tskit,
+    # which is the number of alleles - 1. In the biallelic case this
+    # gives us the number of non-monomorphic sites.
+    S = ((ac > 0).sum(axis=1) - 1).sum()
 
     # assume number of chromosomes sampled is constant for all variants
     # NOTE: even tho ac has dtype uint, we promote the sum to float
@@ -583,14 +585,17 @@ def Tajimas_D(
     c2 = b2 - ((n + 2) / (a1 * n)) + (a2 / (a1 ** 2))
     e1 = c1 / a1
     e2 = c2 / (a1 ** 2 + a2)
-    d_stdev = np.sqrt((e1 * S) + (e2 * S * (S - 1)))
+    d_stdev = da.sqrt((e1 * S) + (e2 * S * (S - 1)))
 
+    # FIXME - this will be an array in general, right?
     if d_stdev == 0:
+        # FIXME to get exactly the same answer as tskit we have to
+        # return either -inf, 0 or +inf depending on whether d is
+        # negative, 0 or positive.
         D = np.nan
+        # D = -np.inf
     else:
-        # finally calculate Tajima's D
         D = d / d_stdev
-
     new_ds = create_dataset({variables.stat_Tajimas_D: D})
     return conditional_merge_datasets(ds, new_ds, merge)
 

sgkit/tests/test_popgen.py
@@ -347,9 +347,7 @@ def test_Fst__windowed(sample_size, n_cohorts, chunks):
         )  # scikit-allel has final window missing
 
 
-# Skipping values > 2, because of this issue:
-# https://github.com/pystatgen/sgkit/issues/522
-@pytest.mark.parametrize("sample_size", [2])
+@pytest.mark.parametrize("sample_size", [2, 3, 5, 10, 100])
 def test_Tajimas_D(sample_size):
     ts = simulate_ts(sample_size)
     ds = ts_to_dataset(ts)  # type: ignore[no-untyped-call]
@@ -359,6 +357,22 @@ def test_Tajimas_D(sample_size):
     ds = Tajimas_D(ds)
     d = ds.stat_Tajimas_D.compute()
     ts_d = ts.Tajimas_D()
+    print("d = ", ts_d)
+    np.testing.assert_allclose(d, ts_d)
+
+
+@pytest.mark.parametrize("sample_size", [2, 3, 5, 10, 100])
+def test_Tajimas_D_per_site(sample_size):
+    ts = simulate_ts(sample_size, random_seed=1234)
+    ds = ts_to_dataset(ts)  # type: ignore[no-untyped-call]
+    ds, subsets = add_cohorts(ds, ts, cohort_key_names=None)  # type: ignore[no-untyped-call]
+    ds = window(ds, size=1)  # by site
+    print(ds)
+    ds = Tajimas_D(ds)
+    d = ds.stat_Tajimas_D.compute()
+    ts_d = ts.Tajimas_D(windows="sites")
+    print("TS = ", ts_d)
+    print("SG = ", np.array(d)[:, 0])
     np.testing.assert_allclose(d, ts_d)
 
 