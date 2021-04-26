4a1924185f53e4076ea78e0609a9fd5bd31a374c
Tom White <tom.e.white@gmail.com>
2021-04-26 15:20:10 +0100

    Compare with scikit-allel

--- sgkit/tests/test_ld.py
+++ sgkit/tests/test_ld.py
@@ -1,20 +1,28 @@
+import allel
 import numpy as np
 
 from sgkit.stats.ld import rogers_huff_r2_between, rogers_huff_r_between
 
 
 def test_rogers_huff_r_between():
-    gna = np.array([0, 1, 2])
-    gnb = np.array([0, 1, 2])
-    assert rogers_huff_r_between(gna, gnb) == 1.0
-    assert rogers_huff_r2_between(gna, gnb) == 1.0
+    gna = np.array([[0, 1, 2]])
+    gnb = np.array([[0, 1, 2]])
+    assert rogers_huff_r_between(gna[0], gnb[0]) == 1.0
+    assert rogers_huff_r2_between(gna[0], gnb[0]) == 1.0
+    assert allel.rogers_huff_r_between(gna, gnb) == rogers_huff_r_between(
+        gna[0], gnb[0]
+    )
 
-    gna = np.array([0, 1, 2])
-    gnb = np.array([2, 1, 0])
-    assert rogers_huff_r_between(gna, gnb) == -1.0
-    assert rogers_huff_r2_between(gna, gnb) == 1.0
+    gna = np.array([[0, 1, 2]])
+    gnb = np.array([[2, 1, 0]])
+    assert rogers_huff_r_between(gna[0], gnb[0]) == -1.0
+    assert rogers_huff_r2_between(gna[0], gnb[0]) == 1.0
+    assert allel.rogers_huff_r_between(gna, gnb) == rogers_huff_r_between(
+        gna[0], gnb[0]
+    )
 
-    gna = np.array([0, 0, 0])
-    gnb = np.array([1, 1, 1])
-    assert np.isnan(rogers_huff_r_between(gna, gnb))
-    assert np.isnan(rogers_huff_r2_between(gna, gnb))
+    gna = np.array([[0, 0, 0]])
+    gnb = np.array([[1, 1, 1]])
+    assert np.isnan(rogers_huff_r_between(gna[0], gnb[0]))
+    assert np.isnan(rogers_huff_r2_between(gna[0], gnb[0]))
+    assert np.isnan(allel.rogers_huff_r_between(gna, gnb))
