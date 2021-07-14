54e967d1a79049752f8da33585d8908a9e1bd72e
Tom White <tom.e.white@gmail.com>
2021-07-14 15:15:23 +0100

    Update setup.py so that GitHub dependency graph works properly #630

--- setup.py
+++ setup.py
@@ -1,4 +1,9 @@
 #!/usr/bin/env python
 from setuptools import setup
 
-setup(use_scm_version=True)
+setup(
+    # The package name along with all the other metadata is specified in setup.cfg
+    # However, GitHub's dependency graph can't see the package unless we put this here.
+    name="sgkit",
+    use_scm_version=True,
+)
