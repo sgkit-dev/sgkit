d31469a53a9d50df8a14f42de0eb4ac74443d4a2
Tom White <tom.e.white@gmail.com>
2021-11-16 14:59:51 +0000

    Add documentation for 0.4.0

---
+++ 0.4.0/_static/docsearch.sbt.js
@@ -0,0 +1,6 @@
+docsearch({
+    apiKey: 'b547668ae472e6a13ae311fb4a8928a3',
+    indexName: 'sgkit',
+    inputSelector: '#search-input',
+    debug: false // Set debug to true if you want to inspect the dropdown
+});
