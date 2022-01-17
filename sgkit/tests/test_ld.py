3f46bef9fb5dc68dff247312bd448d8d03fc47df
Tom White <tom.e.white@gmail.com>
2022-01-17 11:40:57 +0000

    Upgrade to numba 0.55.0

--- sgkit/tests/test_ld.py
+++ sgkit/tests/test_ld.py
@@ -49,11 +49,17 @@ def test_rogers_huff_r_between():
     assert np.isnan(rogers_huff_r2_between(gna[0], gnb[0]))
     assert np.isnan(allel.rogers_huff_r_between(gna, gnb))
 
-    # a case where scikit-allel is different due to its use of float32
+    gna = np.array([[1, 1, 1]])
+    gnb = np.array([[1, 1, 1]])
+    assert np.isnan(rogers_huff_r_between(gna[0], gnb[0]))
+    assert np.isnan(rogers_huff_r2_between(gna[0], gnb[0]))
+    assert np.isnan(allel.rogers_huff_r_between(gna, gnb))
+
+    # a case which fails if fastmath=True is enabled for rogers_huff_r_between
     gna = np.full((1, 49), 2)
     gnb = np.full((1, 49), 2)
-    npt.assert_allclose(rogers_huff_r_between(gna[0], gnb[0]), 1.0, rtol=1e-06)
-    npt.assert_allclose(rogers_huff_r2_between(gna[0], gnb[0]), 1.0, rtol=1e-06)
+    assert np.isnan(rogers_huff_r_between(gna[0], gnb[0]))
+    assert np.isnan(rogers_huff_r2_between(gna[0], gnb[0]))
     assert np.isnan(allel.rogers_huff_r_between(gna, gnb))
 
 
