ccbc8d3fa1a57c9614ad139045cd35a7a02cdc52
Tom White <tom.e.white@gmail.com>
2023-02-09 10:43:09 +0000

    Generate VCF header when writing, if no header is explicitly supplied

--- sgkit/tests/io/vcf/test_vcf_roundtrip.py
+++ sgkit/tests/io/vcf/test_vcf_roundtrip.py
@@ -1,13 +1,14 @@
 import pytest
 from numcodecs import FixedScaleOffset
 
+from sgkit import load_dataset
 from sgkit.io.vcf.vcf_reader import vcf_to_zarr, zarr_array_sizes
-from sgkit.io.vcf.vcf_writer import zarr_to_vcf
+from sgkit.io.vcf.vcf_writer import write_vcf, zarr_to_vcf
 from sgkit.tests.io.vcf.utils import assert_vcfs_close, path_for_test
 
 
 @pytest.mark.parametrize(
-    "vcf_file, encoding",
+    "vcf_file, encoding, generate_header",
     [
         (
             "1kg_target_chr20_38_imputed_chr20_1000.vcf",
@@ -28,22 +29,26 @@ from sgkit.tests.io.vcf.utils import assert_vcfs_close, path_for_test
                     ],
                 },
             },
+            True,
         ),
-        ("all_fields.vcf", None),
-        ("CEUTrio.20.21.gatk3.4.g.vcf.bgz", None),
-        ("Homo_sapiens_assembly38.headerOnly.vcf.gz", None),
-        ("mixed.vcf.gz", None),
-        ("no_genotypes.vcf", None),
-        ("no_genotypes_with_gt_header.vcf", None),
-        ("sample_multiple_filters.vcf.gz", None),
-        ("sample.vcf.gz", None),
+        ("all_fields.vcf", None, True),
+        ("CEUTrio.20.21.gatk3.4.g.vcf.bgz", None, True),
+        # don't generate header to avoid https://github.com/pydata/xarray/issues/7328
+        ("Homo_sapiens_assembly38.headerOnly.vcf.gz", None, False),
+        ("mixed.vcf.gz", None, True),
+        ("no_genotypes.vcf", None, True),
+        ("no_genotypes_with_gt_header.vcf", None, True),
+        ("sample_multiple_filters.vcf.gz", None, True),
+        ("sample.vcf.gz", None, True),
     ],
 )
 @pytest.mark.filterwarnings(
     "ignore::sgkit.io.vcf.FloatFormatFieldWarning",
     "ignore::sgkit.io.vcfzarr_reader.DimensionNameForFixedFormatFieldWarning",
 )
-def test_vcf_to_zarr_to_vcf__real_files(shared_datadir, tmp_path, vcf_file, encoding):
+def test_vcf_to_zarr_to_vcf__real_files(
+    shared_datadir, tmp_path, vcf_file, encoding, generate_header
+):
     path = path_for_test(shared_datadir, vcf_file)
     intermediate = tmp_path.joinpath("intermediate.vcf.zarr").as_posix()
     output = tmp_path.joinpath("output.vcf").as_posix()
@@ -61,3 +66,10 @@ def test_vcf_to_zarr_to_vcf__real_files(shared_datadir, tmp_path, vcf_file, enco
     zarr_to_vcf(intermediate, output)
 
     assert_vcfs_close(path, output)
+
+    if generate_header:
+        # delete VCF header attribute to check
+        # we can generate a VCF header without error
+        ds = load_dataset(intermediate)
+        del ds.attrs["vcf_header"]
+        write_vcf(ds, output)
--- sgkit/tests/io/vcf/test_vcf_writer.py
+++ sgkit/tests/io/vcf/test_vcf_writer.py
@@ -1,6 +1,7 @@
 import gzip
 from io import StringIO
 
+import numpy as np
 import pytest
 from cyvcf2 import VCF
 from numpy.testing import assert_array_equal
@@ -8,8 +9,9 @@ from numpy.testing import assert_array_equal
 from sgkit.io.dataset import load_dataset
 from sgkit.io.vcf.vcf_reader import vcf_to_zarr, zarr_array_sizes
 from sgkit.io.vcf.vcf_writer import write_vcf, zarr_to_vcf
+from sgkit.testing import simulate_genotype_call_dataset
 
-from .utils import path_for_test
+from .utils import assert_vcfs_close, path_for_test
 from .vcf_writer import canonicalize_vcf
 
 
@@ -113,11 +115,14 @@ def test_write_vcf(shared_datadir, tmp_path, in_memory_ds):
         [[10, 15], [10, 10], [3, 3]],
     )
 
+    # check headers are the same
+    assert_vcfs_close(path, output)
+
 
 @pytest.mark.filterwarnings(
     "ignore::sgkit.io.vcfzarr_reader.DimensionNameForFixedFormatFieldWarning",
 )
-def test_write_vcf__drop_fields(shared_datadir, tmp_path):
+def test_write_vcf__set_header(shared_datadir, tmp_path):
     path = path_for_test(shared_datadir, "sample.vcf.gz")
     intermediate = tmp_path.joinpath("intermediate.vcf.zarr").as_posix()
     output = tmp_path.joinpath("output.vcf").as_posix()
@@ -129,17 +134,153 @@ def test_write_vcf__drop_fields(shared_datadir, tmp_path):
 
     ds = load_dataset(intermediate)
 
+    # specified header drops NS and HQ fields,
+    # and adds H3 and GL fields (which are not in the data)
+    vcf_header = """##fileformat=VCFv4.3
+##INFO=<ID=AN,Number=1,Type=Integer,Description="Total number of alleles in called genotypes">
+##INFO=<ID=AC,Number=.,Type=Integer,Description="Allele count in genotypes, for each ALT allele, in the same order as listed">
+##INFO=<ID=DP,Number=1,Type=Integer,Description="Total Depth">
+##INFO=<ID=AF,Number=.,Type=Float,Description="Allele Frequency">
+##INFO=<ID=AA,Number=1,Type=String,Description="Ancestral Allele">
+##INFO=<ID=DB,Number=0,Type=Flag,Description="dbSNP membership, build 129">
+##INFO=<ID=H2,Number=0,Type=Flag,Description="HapMap2 membership">
+##INFO=<ID=H3,Number=0,Type=Flag,Description="HapMap3 membership">
+##FILTER=<ID=s50,Description="Less than 50% of samples have data">
+##FILTER=<ID=q10,Description="Quality below 10">
+##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">
+##FORMAT=<ID=GQ,Number=1,Type=Integer,Description="Genotype Quality">
+##FORMAT=<ID=DP,Number=1,Type=Integer,Description="Read Depth">
+##FORMAT=<ID=GL,Number=G,Type=Float,Description="Genotype likelihoods">
+#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	NA00001	NA00002	NA00003
+"""
+
+    write_vcf(ds, output, vcf_header=vcf_header)
+
+    # check dropped fields are not present in VCF
+    v = VCF(output)
+    assert "##INFO=<ID=NS" not in v.raw_header
+    assert "##FORMAT=<ID=HQ" not in v.raw_header
+    count = 0
+    for variant in v:
+        assert "NS" not in dict(variant.INFO).keys()
+        assert "HQ" not in variant.FORMAT
+        assert variant.genotypes is not None
+        count += 1
+    assert count == 9
+
+
+@pytest.mark.parametrize("generate_header", [True, False])
+@pytest.mark.filterwarnings(
+    "ignore::sgkit.io.vcfzarr_reader.DimensionNameForFixedFormatFieldWarning",
+)
+def test_write_vcf__add_drop_fields(shared_datadir, tmp_path, generate_header):
+    path = path_for_test(shared_datadir, "sample.vcf.gz")
+    intermediate = tmp_path.joinpath("intermediate.vcf.zarr").as_posix()
+    output = tmp_path.joinpath("output.vcf").as_posix()
+
+    kwargs = zarr_array_sizes(path)
+    vcf_to_zarr(
+        path, intermediate, fields=["INFO/*", "FORMAT/*"], mixed_ploidy=True, **kwargs
+    )
+
+    ds = load_dataset(intermediate)
+
+    if generate_header:
+        # delete header attribute and check it is still generated
+        del ds.attrs["vcf_header"]
+
+    # add an INFO field and a FORMAT field
+    ds["variant_AD"] = (
+        ["variants", "alleles"],
+        np.random.randint(
+            50, size=(ds.dims["variants"], ds.dims["alleles"]), dtype=np.int32
+        ),
+    )
+    ds["call_DS"] = (
+        ["variants", "samples", "alt_alleles"],
+        np.random.random(
+            (ds.dims["variants"], ds.dims["samples"], ds.dims["alleles"] - 1)
+        ).astype(np.float32),
+    )
+
     # drop an INFO field and a FORMAT field
     ds = ds.drop_vars(["variant_NS", "call_HQ"])
 
     write_vcf(ds, output)
 
+    # check added fields are present in VCF
+    v = VCF(output)
+    assert (
+        '##INFO=<ID=AD,Number=R,Type=Integer,Description="Total read depth for each allele"'
+        in v.raw_header
+    )
+    assert '##FORMAT=<ID=DS,Number=A,Type=Float,Description=""' in v.raw_header
+    for variant in v:
+        assert "AD" in dict(variant.INFO).keys()
+        assert "DS" in variant.FORMAT
+
     # check dropped fields are not present in VCF
     v = VCF(output)
+    assert "##INFO=<ID=NS" not in v.raw_header
+    assert "##FORMAT=<ID=HQ" not in v.raw_header
     count = 0
     for variant in v:
-        assert "NS" not in variant.INFO
-        assert variant.format("HQ") is None
+        assert "NS" not in dict(variant.INFO).keys()
+        assert "HQ" not in variant.FORMAT
         assert variant.genotypes is not None
         count += 1
     assert count == 9
+
+
+def test_write_vcf__from_non_vcf_source(tmp_path):
+    output = tmp_path.joinpath("output.vcf").as_posix()
+
+    # simulate a dataset
+    ds = simulate_genotype_call_dataset(n_variant=10, n_sample=10, missing_pct=0.3)
+    ds["variant_position"] = ds["variant_position"] + 1  # make 1-based for VCF
+
+    # add an INFO field
+    ds["variant_AD"] = (
+        ["variants", "alleles"],
+        np.random.randint(
+            50, size=(ds.dims["variants"], ds.dims["alleles"]), dtype=np.int32
+        ),
+    )
+
+    # write to VCF with a generated header
+    write_vcf(ds, output)
+
+    # check the header (except for source line, which is version dependent)
+    v = VCF(output)
+    header = v.raw_header.strip()
+    header_lines = [
+        line for line in header.split("\n") if not line.startswith("##source")
+    ]
+    assert header_lines == [
+        "##fileformat=VCFv4.3",
+        '##FILTER=<ID=PASS,Description="All filters passed">',
+        '##INFO=<ID=AD,Number=R,Type=Integer,Description="Total read depth for each allele">',
+        '##FORMAT=<ID=GT,Number=1,Type=String,Description="Genotype">',
+        "##contig=<ID=0>",
+        "#CHROM	POS	ID	REF	ALT	QUAL	FILTER	INFO	FORMAT	S0	S1	S2	S3	S4	S5	S6	S7	S8	S9",
+    ]
+
+
+def test_write_vcf__generate_header_errors(tmp_path):
+    output = tmp_path.joinpath("output.vcf").as_posix()
+
+    # simulate a dataset
+    ds = simulate_genotype_call_dataset(n_variant=10, n_sample=10, missing_pct=0.3)
+    ds["variant_position"] = ds["variant_position"] + 1  # make 1-based for VCF
+
+    # unsupported dtype
+    ds["variant_AB"] = (["variants"], np.zeros(10, dtype="complex"))
+    with pytest.raises(ValueError, match=r"Unsupported dtype: complex"):
+        write_vcf(ds, output)
+
+    # VCF number cannot be determined from dimension name
+    ds["variant_AB"] = (["variants", "my_dim"], np.zeros((10, 7), dtype=np.int32))
+    with pytest.raises(
+        ValueError, match=r"Cannot determine VCF Number for dimension name 'my_dim'"
+    ):
+        write_vcf(ds, output)
