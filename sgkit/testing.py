a4cb51192acf0d46de9a08da26d35821df7800f4
Tom White <tom.e.white@gmail.com>
2023-02-13 14:01:47 +0000

    Allow `simulate_genotype_call_dataset` to add phasing info

--- sgkit/testing.py
+++ sgkit/testing.py
@@ -17,6 +17,7 @@ def simulate_genotype_call_dataset(
     n_contig: int = 1,
     seed: Optional[int] = 0,
     missing_pct: Optional[float] = None,
+    phased: Optional[bool] = None,
 ) -> Dataset:
     """Simulate genotype calls and variant/sample data.
 
@@ -46,7 +47,9 @@ def simulate_genotype_call_dataset(
     seed
         Seed for random number generation, optional
     missing_pct
-        Donate the percent of missing calls, must be within [0.0, 1.0], optional
+        The percentage of missing calls, must be within [0.0, 1.0], optional
+    phased
+        Whether genotypes are phased, default is unphased, optional
 
     Returns
     -------
@@ -58,6 +61,7 @@ def simulate_genotype_call_dataset(
     - :data:`sgkit.variables.sample_id_spec` (samples)
     - :data:`sgkit.variables.call_genotype_spec` (variants, samples, ploidy)
     - :data:`sgkit.variables.call_genotype_mask_spec` (variants, samples, ploidy)
+    - :data:`sgkit.variables.call_genotype_phased_spec` (variants, samples), if ``phased`` is not None
     """
     if missing_pct and (missing_pct < 0.0 or missing_pct > 1.0):
         raise ValueError("missing_pct must be within [0.0, 1.0]")
@@ -69,6 +73,10 @@ def simulate_genotype_call_dataset(
         call_genotype = np.where(
             rs.rand(*call_genotype.shape) < missing_pct, -1, call_genotype
         )
+    if phased is None:
+        call_genotype_phased = None
+    else:
+        call_genotype_phased = np.full((n_variant, n_sample), phased, dtype=bool)
 
     contig_size = split_array_chunks(n_variant, n_contig)
     contig = np.repeat(np.arange(n_contig), contig_size)
@@ -86,4 +94,5 @@ def simulate_genotype_call_dataset(
         variant_allele=alleles,
         sample_id=sample_id,
         call_genotype=call_genotype,
+        call_genotype_phased=call_genotype_phased,
     )
--- sgkit/tests/test_testing.py
+++ sgkit/tests/test_testing.py
@@ -1,5 +1,6 @@
 import re
 
+import numpy as np
 import pytest
 import xarray as xr
 
@@ -9,6 +10,7 @@ from sgkit.testing import simulate_genotype_call_dataset
 def test_simulate_genotype_call_dataset__zarr(tmp_path):
     path = str(tmp_path / "ds.zarr")
     ds = simulate_genotype_call_dataset(n_variant=10, n_sample=10)
+    assert "call_genotype_phased" not in ds
     ds.to_zarr(path)
     xr.testing.assert_equal(ds, xr.open_zarr(path, concat_characters=False))
 
@@ -18,3 +20,9 @@ def test_simulate_genotype_call_dataset__invalid_missing_pct():
         ValueError, match=re.escape("missing_pct must be within [0.0, 1.0]")
     ):
         simulate_genotype_call_dataset(n_variant=10, n_sample=10, missing_pct=-1.0)
+
+
+def test_simulate_genotype_call_dataset__phased(tmp_path):
+    ds = simulate_genotype_call_dataset(n_variant=10, n_sample=10, phased=True)
+    assert "call_genotype_phased" in ds
+    assert np.all(ds["call_genotype_phased"])
