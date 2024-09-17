3ff53ffafc247950862b7ebbbef1b754851c2719
Tom White <tom.e.white@gmail.com>
2024-09-17 17:11:58 +0100

    Remove sgkit/tests/io/vcf

--- sgkit/tests/io/vcf/__init__.py
+++
@@ -1,6 +0,0 @@
-import pytest
-
-cyvcf2 = pytest.importorskip("cyvcf2")  # noqa: F401
-
-# rewrite asserts in assert_vcfs_close to give better failure messages
-pytest.register_assert_rewrite("sgkit.tests.io.vcf.utils")
