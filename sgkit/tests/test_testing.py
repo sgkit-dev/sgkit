8f3703f8e2f94fb3b1a8126ddb62d39edbffa13a
Tom White <tom.e.white@gmail.com>
2025-01-14 10:24:56 +0000

    Fixes for zarr-python 3.0.0

--- sgkit/tests/test_testing.py
+++ sgkit/tests/test_testing.py
@@ -7,6 +7,9 @@ import xarray as xr
 from sgkit.testing import simulate_genotype_call_dataset
 
 
+@pytest.mark.filterwarnings(
+    "ignore::UserWarning"
+)  # codec `vlen-utf8` not in Zarr v3 spec`
 def test_simulate_genotype_call_dataset__zarr(tmp_path):
     path = str(tmp_path / "ds.zarr")
     ds = simulate_genotype_call_dataset(n_variant=10, n_sample=10)
