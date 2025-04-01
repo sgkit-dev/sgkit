0aeb7c6e9953417d0bc7ef57080629b1165b6970
Ian Hunt-Isaak <ianhuntisaak@gmail.com>
2025-04-01 18:26:30 -0400

    build: switch to pyproject.toml
    Previously pip was generating a warning when trying to do an editable
    install

--- setup.py
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env python
-from setuptools import setup
-
-setup(
-    # The package name along with all the other metadata is specified in setup.cfg
-    # However, GitHub's dependency graph can't see the package unless we put this here.
-    name="sgkit",
-    use_scm_version=True,
-)
