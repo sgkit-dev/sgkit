7fdce788cc248278f21b31fccaed300714078769
Tom White <tom.e.white@gmail.com>
2025-04-01 14:17:58 +0100

    Fix Cubed tests

--- sgkit/stats/pca.py
+++ sgkit/stats/pca.py
@@ -325,7 +325,7 @@ def count_call_alternate_alleles(ds: Dataset, merge: bool = True) -> Dataset:
     # TODO: Add to public API (https://github.com/sgkit-dev/sgkit/issues/282)
     AC = count_call_alleles(ds)["call_allele_count"]
     AC = AC[..., 1:].sum(dim="alleles").astype("int16")
-    AC = AC.where(~ds.call_genotype_mask.any(dim="ploidy"), AC.dtype.type(-1))
+    AC = AC.where(~ds.call_genotype_mask.any(dim="ploidy"), -1)
     new_ds = Dataset({"call_alternate_allele_count": AC})
     return conditional_merge_datasets(ds, new_ds, merge)
 
